e145487d13844feeb0b758d630f0bef45c3df5bb
Marie8bit <yd7581ku@go.minneapolis.edu>
2016-11-08 00:18:08 -0600

    works for two players

--- src/com/MarieErickson/Main.java
+++ src/com/MarieErickson/Main.java
@@ -4,17 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int winner = 0;
         Deck deck = new Deck();
         System.out.println(deck);
         //todo allow for choice of numbers of human players/comp players?
         // vaidate/max 5
-        PlayerManager plMgmt = new PlayerManager(2, deck);
+        PlayerManager plMgmt = new PlayerManager(5, deck);
         System.out.println(deck);
         for (Player plyr:plMgmt.players){
             System.out.println(plyr.hand.toString());
         }
         for (int z= 0;z<6;z++){
-        plMgmt.round();
+        winner = plMgmt.round();
         }
+        System.out.println("Player"+winner+" won the game");
     }
 }
--- src/com/MarieErickson/PlayerManager.java
+++ src/com/MarieErickson/PlayerManager.java
@@ -1,6 +1,7 @@
 package com.MarieErickson;
 
 import java.util.LinkedList;
+import java.util.Objects;
 
 /**
  * Created by Marie on 10/25/2016.
@@ -9,9 +10,10 @@ public class PlayerManager {
     LinkedList<Player> players = new LinkedList<>();
     public final int MAX_PLAYERS = 5;
     private int starter;
+    private int lastRoundWinner;
 
     public PlayerManager(int num, Deck deck) {
-        for (int x = 1; x-1 < num; x++) {
+        for (int x = 1; x <= num; x++) {
             Player ply = new Player(x);
             players.add(ply);
         }
@@ -25,7 +27,7 @@ public class PlayerManager {
                 dealRounds++;
             }
         }
-        this.starter=1;
+        this.starter=0;
     }
 
     public int getStarter() {
@@ -36,30 +38,105 @@ public class PlayerManager {
         this.starter = starter;
     }
 
-    public void round() {
-        for (Player plr:players){
-            if (plr.playerName == starter) {
-                Card lead = lead(plr);
+    public int round() {
+        int round = 1;
+        LinkedList<Player>roundList = new LinkedList<>(this.players);
+        LinkedList<Card> roundCardList = new LinkedList<>();
+        Card leadCard;
+        Player plr =roundList.get(starter);
+        leadCard = lead(plr);
+        roundCardList.add(leadCard);
+        System.out.println("Player"+roundList.get(0).playerName+" played "+leadCard);
+        roundList.remove(plr);
+
+
+        for (int x=0;(roundList.size()!=0);(x)++){
+            for(Player pl : roundList){
+                if (pl.playerName==x+starter){
+                    Card flw= follow(pl,leadCard);
+                    System.out.println("Player"+roundList.get(0).playerName+" played "+flw);
+                    roundCardList.add(flw);
+                    roundList.remove(pl);
+
+                    if(flw.value>leadCard.value&& Objects.equals(flw.suit, leadCard.suit)){
+                        leadCard=flw;
+                    }
+
+                }
+                else {
+                break;}
             }
-        }
         //todo add next players action
         //for (players)
         //follow(player, lead)
-    }
+        }
+        int winValue = 0;
+        for (Card crd: roundCardList) {
+            if (Objects.equals(crd.suit, leadCard.suit)) {
+                if (crd.value > winValue) {
+                    winValue = crd.value;
+                }
+            }
+        }
+            for (Player o:players){
+                for (Card a:o.hand){
+                    if(a.value==winValue&&Objects.equals(a.suit, leadCard.suit)){
+                        System.out.println("Player"+o.playerName+" won round"+round);
+                        round++;
+                        this.lastRoundWinner = o.playerName;
+                    }
+                }
+            }
+
+            for (Player p: players){
+                for (Card cardd:roundCardList){
+                    if (p.hand.contains(cardd)){
+                        p.hand.remove(cardd);
+                    }
+                }
+
+            }
+            return lastRoundWinner;
+        }
+
+
     public Card lead(Player player){
 
         int min=12;
+        Card leadCard= player.hand.get(0);
         for (Card c:player.hand){
             if (c.value<min)
-            {min=c.value;
+            {
+                min=c.value;
+                leadCard=c;
             }
+
         }
         //todo remove card from player hand
-        return player.hand.get(min);
+        return leadCard;
     }
-    public Card follow(Player player, Card lead){
-        int flw= 0;
-        return player.hand.get(flw);
+    public Card follow(Player player, Card leadCard){
+        LinkedList<Card> playerRoundCardList = new LinkedList<>();
+        Card flw= player.hand.get(0);
+        for (Card c:player.hand) {
+            if (c.suit.equals(leadCard.suit)){
+                playerRoundCardList.add(c);
+            }
+        }
+        if (playerRoundCardList.size()!=0){
+            for (Card l:playerRoundCardList){
+                if (l.value>leadCard.value){
+                    flw=l;
+                }
+            }
+        }
+        else {
+            for (Card m:player.hand){
+                if (m.value<flw.value)
+                {flw=m;}
+            }
+        }
+        return flw;
     }
 }
 
